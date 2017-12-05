cc48367491b73ab561c26f569d52a8ea420772d1
H Phil Duby <philduby@phriendly.net>
2017-12-05 14:29:23 -0700

    Replace hardcode photobioreactor device id with an imported station id
    Cleanup for pylint and pycodestyle
    Prepare to use json libary to generation measurment record

--- genericsensor.py
+++ genericsensor.py
@@ -1,16 +1,22 @@
+"""
+Generic top level Sensor class to be extended by all actual sensors.
+"""
+
 import threading
-import datetime
+# import json
+import photobioreactor
 
 
 class Sensor(object):
     """
-    Basic sensor object that all others should extend. Provides support for collecting sensor values, monitoring a
-    sensor for changes, and writing sensor values to file to be picked up by the send_json_to_server.sh script.
+    Basic sensor object that all others should extend.  Provides support for
+    collecting sensor values, monitoring a sensor for changes, and writing
+    sensor values to a file.
 
-    All extending classes will need to implement get_value() or _monitor_value().
+    All extending classes will need to implement get_value() or
+    _monitor_value().
     """
     json_folder = '/usr/local/bin/BioReactor/'
-    device_id = '00002'
 
     def __init__(self):
         self.continue_monitor = False
@@ -18,25 +24,29 @@ class Sensor(object):
 
     def get_value(self):
         """
-        Gets the current value of the sensor.
+        Return the current sensor value.
+
         :return: The value for the sensor at the current time.
         """
-        raise NotImplementedError("Get value method for sensor not yet implemented.")
+        raise NotImplementedError(
+            "Get value method for sensor not yet implemented.")
 
     def start_monitor(self, callback):
         """
-        Starts the sensor monitor with the given callback.
+        Start the sensor monitor with the given callback.
         """
         if self.monitor_thread is None or not self.monitor_thread.is_alive():
             self.continue_monitor = True
-            self.monitor_thread = threading.Thread(target=self._monitor_value, args=(callback,))
+            self.monitor_thread = threading.Thread(
+                target=self._monitor_value, args=(callback,))
             self.monitor_thread.start()
 
     def stop_monitor(self, wait_for_stop=False):
         """
-        Stops the monitor thread.
-        :param wait_for_stop: Whether the method should wait for the monitor to stop before returning. If false, will
-                              return immediately.
+        Stop the monitor thread.
+
+        :param wait_for_stop: Whether the method should wait for the monitor to
+            stop before returning. If false, will return immediately.
         """
         self.continue_monitor = False
         if wait_for_stop:
@@ -44,42 +54,63 @@ class Sensor(object):
 
     def wait_monitor(self):
         """
-        Causes the calling thread to wait/sleep for as long as the monitor is running. Useful for when you want to start
-        a monitor and have no need to stop it.
+        Cause the calling thread to wait/sleep for as long as the monitor is
+        running.  Useful for when you want to start a monitor and have no need
+        to stop it.
         """
         if self.monitor_thread is not None and self.monitor_thread.is_alive():
             self.monitor_thread.join()
 
     def _monitor_value(self, callback):
         """
-        Watches a sensor for a trigger behavior that when seen, will run the given callback function with the sensor
-        value or state as a parameter. This should have an infinite loop that runs as long as continue_monitor is true.
+        Watch a sensor for a trigger behavior that when seen, will run the
+        given callback function with the sensor value or state as a parameter.
+        This should have an infinite loop that runs as long as continue_monitor
+        is true.
+
         :param callback:
         :return:
         """
-        raise NotImplementedError("Monitor value method for sensor not yet implemented.")
+        raise NotImplementedError(
+            "Monitor value method for sensor not yet implemented.")
 
     @staticmethod
     def generate_file(value_name, value, json_prefix, json_time):
         """
-        Takes a sensor value and current time and creates a json file ready to be sent to the server.
-        :param value_name: The name the server expects to be paired with the sensor value. E.g. 'temperature' or 'lux'.
-        :param value: The value to send to the server. If not already a string, will be convered to one with str(value)
-        :param json_prefix: The prefix to use for the json file name. E.g. 'bio_temp' or 'bio_light'
-        :param json_time: The time to use for the json file name as well as the json 'recorded_on' parameter.
+        Takes a sensor value and current time and creates a json file ready to
+        be sent to the server.
+
+        :param value_name: The name the server expects to be paired with the
+            sensor value. E.g. 'temperature' or 'lux'.
+        :param value: The value to send to the server. If not already a string,
+            will be convered to one with str(value)
+        :param json_prefix: The prefix to use for the json file name. E.g.
+            'bio_temp' or 'bio_light'
+        :param json_time: The time to use for the json file name as well as the
+            json 'recorded_on' parameter.
         """
 
-        file_time = json_time.strftime( '%Y%m%d_%H%M%S' )
-        recorded_on = json_time.strftime( '%Y-%m-%d %H:%M:%S' )
-        json_file = '{}{}_{}.json'.format(Sensor.json_folder, json_prefix, file_time)
+        file_time = json_time.strftime('%Y%m%d_%H%M%S')
+        recorded_on = json_time.strftime('%Y-%m-%d %H:%M:%S')
+        json_file = '{}{}_{}.json'.format(
+            Sensor.json_folder, json_prefix, file_time)
         value_str = str(value)
 
+        # Setup object to convert to json
+        # sensor_data = {}
+        # sensor_data['deviceid'] = photobioreactor.station_id
+        # sensor_data[value_name] = value_str
+        # sensor_data['recorded_on'] = recorded_on
+
         print("Starting write to file: ", json_file)
-        with open(json_file, 'w') as f:
-            file_str = '[{' + '"deviceid":"{}","{}":"{}","recorded_on":"{}"'.format(Sensor.device_id, value_name, value_str, recorded_on) +'}]\n'
-            f.write(file_str)
+        with open(json_file, 'w') as jfile:
+            file_str = '[{' + str.format(
+                '"deviceid":"{}","{}":"{}","recorded_on":"{}"',
+                photobioreactor.station_id, value_name, value_str,
+                recorded_on) + '}]\n'
+            jfile.write(file_str)
+            # json.dump([sensor_data], jfile)
         # Append data value to log.
-        log_file = "datalog/"+json_prefix+".csv"
+        log_file = "datalog/" + json_prefix + ".csv"
         with open(log_file, 'a') as log_f:
-            log_f.write(recorded_on + "," + value_str + "\n")
-        
+            log_f.write('{},{}\n'.format(recorded_on, value_str))
