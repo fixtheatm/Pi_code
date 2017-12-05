1281be3b766bf33dbbc25e7a4d3da913dbe12102
H Phil Duby <philduby@phriendly.net>
2017-12-05 15:27:06 -0700

    Replace hadcoded temperature sensor device id with import
    Cleanup for pylint andn pycodestyle

--- sensor_temperature.py
+++ sensor_temperature.py
@@ -1,35 +1,52 @@
 #!/usr/bin/python3
-from genericsensor import Sensor
+
+"""
+Handle a one wire temperature sensor (DS18B20)
+"""
+
 from datetime import datetime
+from genericsensor import Sensor
+import temperature
 
 
 class TemperatureSensor(Sensor):
-    """Collects temperature sensor values and outputs them to a json file for transfer to server."""
-    def __init__(self):
-        super().__init__()
-        self.device = '/sys/bus/w1/devices/28-0000075d2009/w1_slave'
+    """Collect temperature sensor value"""
+    VALUE_NAME = 'temperature'
+    JSON_PREFIX = 'bio_temp'
+    MEASUREMENT_FMT = '{:4.1f}'
 
     def get_value(self):
-        with open(self.device, 'r') as f:
-            data = f.readlines()
+        with open(temperature.device, 'r') as owfile:
+            data = owfile.readlines()
             deg_c = ''
             if data[0].strip()[-3:] == 'YES':
                 temp = data[1][data[1].find('t=') + 2:]
                 try:
-                    if float(temp) == 0:
+                    ftemp = float(temp)
+                    if ftemp == 0:
                         deg_c = 0
                     else:
-                        deg_c = (float(temp) / 1000)
-                except:
+                        deg_c = ftemp / 1000
+                except ValueError:
                     print("Error with t=", temp)
-                    pass
             return deg_c
 
     def _monitor_value(self, callback):
-        raise NotImplementedError("Temperature sensor does not support monitoring.")
+        raise NotImplementedError(
+            "Temperature sensor does not support monitoring.")
 
-if __name__ == '__main__':
+
+def main():
+    """Code to run when module is executed standalone"""
     sensor = TemperatureSensor()
     value = sensor.get_value()
     print(value)
-    sensor.generate_file( 'temperature', "{:4.1f}".format( value ), 'bio_temp', datetime.utcnow())
+    sensor.generate_file(
+        TemperatureSensor.VALUE_NAME,
+        TemperatureSensor.MEASUREMENT_FMT.format(value),
+        TemperatureSensor.JSON_PREFIX,
+        datetime.utcnow())
+
+
+if __name__ == '__main__':
+    main()
