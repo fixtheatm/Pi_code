cd79308b1276bc1d2117cd024dbada901ceacaad
H. Phil Duby <philduby@phriendly.net>
2018-03-20 15:41:57 -0600

    Remove PH sensor data send; change server to pbr

--- send_json_to_server.sh
+++ send_json_to_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# This is a test script to send the JSON datafiles from the
+# This script sendS the JSON datafiles from the
 #  BioReactor system to the server. It scans the data folder
 #  and sends each json file to the server via the httpie
 #  package. The return code is checked to make sure that the
@@ -15,31 +15,32 @@
 # Linux is raspberry pi production
 OS=$OSTYPE
 Linux=false
-if [[ ${OS:0:5} = "Linux" || ${OS:0:5} = "linux" ]] 
+if [[ ${OS:0:5} = "Linux" || ${OS:0:5} = "linux" ]]
 then
-	Linux=true
+  Linux=true
 fi
 
 # name of the server we are posting to.
 #
-SERVER="2Connect2Biz.com"
+# SERVER="2Connect2Biz.com"
+SERVER="pbr.fixingtheatmosphere.org"
 
-# root folder where are json files are being stored
+# root folder where our json files are being stored
 # on Windows we are using "./" On the raspberry pi it will be "/usr/local/bin/bioreactor/"
 # It needs to end in a forward slash
 ROOT_FOLDER="./"
 if ( $Linux )
 then
-	ROOT_FOLDER="/usr/local/bin/BioReactor/"
+  ROOT_FOLDER="/usr/local/bin/BioReactor/"
 fi
 
 # the wildcard strings denoting our json files
 JSON_TEMP_WILDCARD="bio_temp*.json"
 JSON_GAS_WILDCARD="bio_gas*.json"
 JSON_LIGHT_WILDCARD="bio_light*.json"
-JSON_PH_WILDCARD="bio_ph*.json"
+# JSON_ERR_WILDCARD="bio_error*.json"
 
-# the folder where we are moving the json files that have been 
+# the folder where we are moving the json files that have been
 # successfully posted to the server
 # for Windows we are using "./sent/" On the raspberry pi we are using "/usr/local/bin/bioreactor/sent/"
 # It needs to end in a forward slash
@@ -58,21 +59,21 @@ LOG_FILE=$ROOT_FOLDER$LOG_FILE
 #
 function moveJsonToSentFolder() {
 
-	local fname=$1
-    local logdate=$2
+  local fname=$1
+  local logdate=$2
 
-	local filename=$(basename "$fname")
-    local extension="${filename##*.}"
-    local fileroot="${filename%.*}"
+  local filename=$(basename "$fname")
+  local extension="${filename##*.}"
+  local fileroot="${filename%.*}"
 
-	# tack on the datetime to the sent filename
-	local newname="$SENT_FOLDER$fileroot"_"$logdate.$extension"
+  # tack on the datetime to the sent filename
+  local newname="$SENT_FOLDER$fileroot"_"$logdate.$extension"
 
-	#echo $newname
-	#echo "filename[$filename] extension [$extension] fileroot [$fileroot]"
+  #echo $newname
+  #echo "filename[$filename] extension [$extension] fileroot [$fileroot]"
 
-	echo "$logdate Moving [$fname] to Sent Folder as [$newname]" >> $LOG_FILE
-	mv $fname $newname 2>> $LOG_FILE
+  echo "$logdate Moving [$fname] to Sent Folder as [$newname]" >> $LOG_FILE
+  mv $fname $newname 2>> $LOG_FILE
 }
 
 
@@ -82,24 +83,25 @@ function moveJsonToSentFolder() {
 #
 function sendJsonToServer() {
 
-	local fname=$1
-	local route=$2
-    	local logdate=$(date +"%Y_%m_%d_%H_%M_%S")
+  local fname=$1
+  local route=$2
+  local logdate=$(date +"%Y_%m_%d_%H_%M_%S")
 
-	echo "$logdate Posting ["$fname"] to Server with route ["$route"]" >> $LOG_FILE
+  echo "$logdate Posting ["$fname"] to Server with route ["$route"]" >> $LOG_FILE
 
-	if http --check-status --timeout=2.5 POST http://$SERVER/$route < $fname &> /dev/null; then
-		moveJsonToSentFolder "$fname" "$logdate"
-	else
-		case $? in
-			2) echo "$logdate Request timed out!" >> $LOG_FILE ;;
-			3) echo "$logdate Unexpected HTTP 3xx Redirection!" >> $LOG_FILE ;;
-			4) echo "$logdate HTTP 4xx Client Error!" >> $LOG_FILE ;;
-			5) echo "$logdate HTTP 5xx Server Error!" >> $LOG_FILE ;;
-			6) echo "$logdate Exceeded --max-redirects=<n> redirects!" >> $LOG_FILE ;;
-			*) echo "$logdate Other Error!" >> $LOG_FILE ;;
-		esac
-	fi
+  if http --check-status --timeout=2.5 POST http://$SERVER/$route < $fname &> /dev/null; then
+    moveJsonToSentFolder "$fname" "$logdate"
+  else
+    local rsltstat=$?
+  case rsltstat in
+    2) echo "$logdate Request timed out!" >> $LOG_FILE ;;
+    3) echo "$logdate Unexpected HTTP 3xx Redirection!" >> $LOG_FILE ;;
+    4) echo "$logdate HTTP 4xx Client Error!" >> $LOG_FILE ;;
+    5) echo "$logdate HTTP 5xx Server Error!" >> $LOG_FILE ;;
+    6) echo "$logdate Exceeded --max-redirects=<n> redirects!" >> $LOG_FILE ;;
+    *) echo "$logdate Other Error! $rsltstat" >> $LOG_FILE ;;
+  esac
+  fi
 }
 
 #echo "["$OSTYPE"]["$Linux"]"
@@ -112,20 +114,15 @@ shopt -s nullglob
 # cycle through all the json bioreactor files in the folder
 for f in $ROOT_FOLDER$JSON_TEMP_WILDCARD
 do
-	sendJsonToServer $f pitemp
+  sendJsonToServer $f pitemp
 done
 for f in $ROOT_FOLDER$JSON_GAS_WILDCARD
 do
-	sendJsonToServer $f pigasflow
+  sendJsonToServer $f pigasflow
 done
 for f in $ROOT_FOLDER$JSON_LIGHT_WILDCARD
 do
-	sendJsonToServer $f pilight
-done
-for f in $ROOT_FOLDER$JSON_PH_WILDCARD
-do
-	sendJsonToServer $f piph
+  sendJsonToServer $f pilight
 done
 
 exit 0
-
